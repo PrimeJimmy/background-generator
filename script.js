85ee60ad0901dd2a217270e49e700e8f7be164d5
James Lawson <39174577+PrimeJimmy@users.noreply.github.com>
2018-06-06 03:26:19 -0400

    changed background title

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2= document.querySelector(".color2");
 var body = document.getElementById("gradient");
+var randomColor = document.getElementById('random');
+
+
+
 
 function setGradient () {
 	body.style.background = "linear-gradient(to right, "
@@ -13,7 +17,39 @@ function setGradient () {
 	css.textContent = body.style.background + ";";
 }
 
+function randomBackground () {
+	let randomHue1 = 'rgb(' + (Math.floor(Math.random() * 256)) 
+	+ ',' 
+	+ (Math.floor(Math.random() * 256)) 
+	+ ',' 
+	+ (Math.floor(Math.random() * 256)) 
+	+ ')';
+	let randomHue2 = 'rgb(' + (Math.floor(Math.random() * 256)) 
+	+ ',' 
+	+ (Math.floor(Math.random() * 256)) 
+	+ ',' 
+	+ (Math.floor(Math.random() * 256)) 
+	+ ')';
+
+		function setGradient2 () {
+			body.style.background = "linear-gradient(to right, "
+			 + randomHue1
+			 + ", " 
+			 + randomHue2 
+			 + ")";
+
+			css.textContent = body.style.background + ";";
+		}
+	setGradient2();
+}
+
+
+setGradient();
 
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
+
+randomColor.addEventListener("click", randomBackground);
+
+
